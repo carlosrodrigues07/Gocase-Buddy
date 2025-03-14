9a4f4e8ade40fb39d3261f12ae345c11038510c3
Carlos Henrique <carlospaixao05@gmail.com>
2025-03-14 10:46:53 -0300

    🎉 feat: adicionando intent para saber endereço (app/intents/saber_endereco.py)

---
+++ app/intents/saber_endereco.py
@@ -0,0 +1,7 @@
+# saber_endereco.py
+def handle_saber_endereco():
+    """
+    Lida com a solicitação de endereço da Gocase.
+    """
+    endereco = "A Gocase está localizada em Fortaleza, Ceará, no endereço: Rua dos Acessórios, 123, Bairro Personalizado, CEP 60000-000."
+    return endereco
